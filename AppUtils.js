f192b6e416a3083d2a6a75e6f7f016057dfcf6d4
rgravador <redgiegravador@gmail.com>
2023-05-04 14:58:53 +0800

    added

--- AppUtils.js
+++ AppUtils.js
@@ -1,7 +1,17 @@
+/**
+ * 
+ * @param {*} length of the string
+ * @returns a random string
+ */
 const randomHexString = (length = 12) => {
     return [...Array(length)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
 }
 
+/**
+ * 
+ * @param {*} string to count
+ * @returns an object with letters as keys and counts as values
+ */
 const occurenceCountOfLetterInString = (string) => {
     return string.split("").reduce((acc, letter) => {
         letter = letter.trim().toLowerCase();
@@ -9,3 +19,26 @@ const occurenceCountOfLetterInString = (string) => {
         return acc;
     }, {})
 }
+/**
+ * 
+ * @param {*} string source
+ * @param {*} toFind string that we are looking for
+ * @param {*} replacement string to replace
+ * @returns a string with the replacements
+ * Note: Regex => gi means case-insensitive
+ *       Regec => g  means case-sensitive
+ */
+const textReplace = (string, toFind, replacement) => {
+    var regex = new RegExp(toFind, "gi");
+    return string.replace(regex, replacement);
+}
+
+/**
+ * @function sanitize
+ * @description prevent xss ingestion
+ * @param {string} str
+ */
+const sanitize = (str) => {
+    return str.toString().replace(/</g, '&lt;').replace(/>/g, '&gt;');
+}
+console.log(sanitize('hello world > dhjkfa'))
